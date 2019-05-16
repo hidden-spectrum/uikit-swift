0ff034cbebed9eaf5c8aceea3f6b54d9c73c5b41
Andrew Theis <andrewtheis@icloud.com>
2019-05-15 21:54:33 -0500

    Add ItemContainer type

---
+++ UIKitSwift/Sources/ItemContainer.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright © 2019 Theis Holdings, LLC. All rights reserved.
+//
+
+import Foundation
+
+
+/// A basic protocol that defines something that contains arbitrary items.
+public protocol ItemContainer {
+    associatedtype Item: Equatable
+    var items: [Item] { get }
+}
+
+
+/// Contains an arbitrary array of Equatable items.
+public struct Section<T: Equatable>: ItemContainer {
+    public let items: [T]
+}
+
+
+public extension Array where Element: ItemContainer {
+    
+    /// Looks up the index of the `ItemContainer` in an array that contains the given item.
+    ///
+    /// - Parameter item: The item to search for.
+    /// - Returns: The first index of the `ItemContainer` containing the item, or nil.
+    func indexContaining(item: Element.Item) -> Int? {
+        return self.firstIndex(where: { element -> Bool in
+            return element.items.contains(item)
+        })
+    }
+}
